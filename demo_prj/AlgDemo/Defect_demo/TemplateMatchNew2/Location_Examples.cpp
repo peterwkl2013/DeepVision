0d178ab4d38d059541d0febb8895751de308556b
peterwkl2013 <514704465@qq.com>
2020-10-07 09:35:05 +0800

    update

--- demo_prj/AlgDemo/Defect_demo/TemplateMatchNew2/Location_Examples.cpp
+++ demo_prj/AlgDemo/Defect_demo/TemplateMatchNew2/Location_Examples.cpp
@@ -3,10 +3,8 @@
 	 template detection algorithm examples
 	              
 @BY X-VISION
-@2020/03/26
+@2020/09/26
 *****************************************************/
-
-
 
 #include <vector>
 #include <fstream>
@@ -17,43 +15,13 @@ using namespace cv;
 using namespace std;
 
 static RNG rng;
-
-struct mvTemplateFeature
+ 
+/* match result */
+typedef struct transmat
 {
-	int x;         /* x offset */
-	int y;         /* y offset */
-	int label;     /* Quantization */
-	mvTemplateFeature() : x(0), y(0), label(0) {}
-	mvTemplateFeature(int _x, int _y, int _label) : x(_x), y(_y), label(_label) {};
-};
-
-typedef struct
-{
-	int width;
-	int height;
-	int x_offset;    /* feature offset x */
-	int y_offset;    /* feature offset y */
-	int pyramid_level;
-	std::vector<mvTemplateFeature>features;
-}mvTemplate;
-
-typedef struct
-{
-	vector<float> scale_ranges;      /* scale factor(0.8~1.5) for training,  now only support scale = 1.0 */
-	vector<float> angle_ranges;      /* rotation angle (0~360) for training,  default(0,360) */
-	int total_id;                    /*total template id */
-	float angle_step;                /* angle step */
-	int   padding;                   /* padding size */
-	Point2f tempInfo[4];             /* template location in tempalte image */
-	Point2f objbox[4];               /* box of the template when angle = 0 */
-	Point2f xy_offset;               /* template offset to the image */
-	float width;                     /* original width of template */
-	float height;                    /* original height of template */
-	int num_features;                /* num features */
-	int num_detected;                /* num have detect */
-}TemplateExtraParam;
-
-typedef std::map<String, TemplateExtraParam> TemplateExtraParamMap;
+	int type;             /* 0: 2x3, 1: 3x3, 2: 4x4 */
+	double pdata[16];     /* transform matrix 2x3, 3x3, 4x4 */
+}mvTransfor;
 
 typedef struct
 {
@@ -67,17 +35,24 @@ typedef struct
 
 typedef struct
 {
-	String class_id;    /* class id */
-	int index;          /* template index */
-	float score;        /* score */
-	float angle;        /* angle of this matched object*/
-	float scale;        /* scale factor */
-	Point2f r_center;   /* rotation center */
-	Rect  rect;         /* rectangle location */
-	mvFBox bbox;        /* box olcation */
+	String class_id;         /* class name */
+	int index;               /* template index */
+	int obj_index;           /* object index */
+	float score;             /* score */
+	float angle;             /* angle of this matched object*/
+	float scale;             /* scale factor */
+	Point2f r_center;        /* rotation center */
+	Rect  rect;              /* rectangle location */
+	mvFBox bbox;             /* box olcation */
 	int x_offset, y_offset;
-	int roi_index;       /* roi index */
-	int valid;          /* roi valid */
+	int roi_index;            /* roi index */
+	int valid;                /* roi valid */
+	mvTransfor tmat;          /* transform mat */
+	int track_enable;         /* track enable */
+	int is_track;             /* is beging tracking */
+	int track_id;             /* is beging tracking */
+	int obj_data;             /* object data */
+	vector<Point>track_pnts;  /* traking center point */
 }matchResult;
 
 class  mv2DTemplateMatch
@@ -212,23 +187,23 @@ void DrawCoordTemplate(cv::Mat& show_img, const matchResult& obj, float scale)
 
 	Point2f p1, p2;
 
-	p2 = obj.r_center;
+	p2 = obj.bbox.center;
 	p2.x += ARROW_LENGHT;
-	cv::line(show_img, obj.r_center, p2, cv::Scalar(0, 0, 255), 3);
-	DrawArrow(show_img, obj.r_center, p2, cv::Scalar(0, 0, 255));
+	cv::line(show_img, obj.bbox.center, p2, cv::Scalar(0, 0, 255), 3);
+	DrawArrow(show_img, obj.bbox.center, p2, cv::Scalar(0, 0, 255));
 
 
-	p2 = obj.r_center;
+	p2 = obj.bbox.center;
 	p2.y -= ARROW_LENGHT;
 
-	cv::line(show_img, obj.r_center, p2, cv::Scalar(0, 255, 0), 3);
-	DrawArrow(show_img, obj.r_center, p2, cv::Scalar(0, 255, 0));
+	cv::line(show_img, obj.bbox.center, p2, cv::Scalar(0, 255, 0), 3);
+	DrawArrow(show_img, obj.bbox.center, p2, cv::Scalar(0, 255, 0));
 
-	cv::circle(show_img, obj.r_center, 3, cv::Scalar(0, 255, 255), -1);
+	cv::circle(show_img, obj.bbox.center, 3, cv::Scalar(0, 255, 255), -1);
 	char str[128]{ 0 };
 
-	sprintf_s(str, "(%d,%d)", obj.r_center.x, obj.r_center.y);
-	cv::putText(show_img, str, obj.r_center, CV_FONT_HERSHEY_COMPLEX_SMALL, 0.4 / scale, cv::Scalar(255, 255, 0), 0.5 / scale);
+	sprintf_s(str, "(%d,%d)", obj.bbox.center.x, obj.r_center.y);
+	cv::putText(show_img, str, obj.bbox.center, CV_FONT_HERSHEY_COMPLEX_SMALL, 0.4 / scale, cv::Scalar(255, 255, 0), 0.5 / scale);
 }
 
 
@@ -250,16 +225,16 @@ type = 1,  calc the template image map point
 */
 void mvMap2DPoint(mvFPoint inp, double *hmat, int type, mvFPoint &outp);
 
-/* 应用EXAMPLE程序开始 */
 int main(int argc, char * argv[])
 {
+
 	int ret;
 	mv2DTemplateMatch  my_templ;
 	vector<matchResult>mres;
 
 	/* 算法处理尺度 scl */
-	float scl     = 1;
-	float conf    = 0.6;
+	float scl = 1.0;
+	float conf = 0.8f;
 	int trainFlag = 1;
 
 	/* 算法参数设置 */
@@ -267,14 +242,26 @@ int main(int argc, char * argv[])
 	my_templ.filter_size = 3;
 	my_templ.num_features = 64;
 	my_templ.angle_step = 1;
-	my_templ.sel_mode = 0;
+	my_templ.sel_mode = 1;
 
+	String ssss = "11,22,33,44,55";
+	vector <String>rssss;
 
-	char *path1 = "../../images/bd2_750x1024.bmp";
-	char *path2 = "../../images/bd2_750x1024.bmp";
+	char *tokenptr = strtok((char*)ssss.c_str(), ",");
+	while (tokenptr != NULL)
+	{
+		std::string tk = tokenptr;
+		rssss.emplace_back(tk);
+		tokenptr = strtok(NULL, ",");
+	}
+
+	const char *path1 = "D:\\bd2_750x1024b.bmp";
+	const char *path2 = "D:\\bd2_750x1024b.bmp";
 
 	Mat tmpl = imread(path1);
 	Mat det_img1 = imread(path2);
+
+	//test_roi(tmpl);
 
 	resize(det_img1, tmpl, Size(det_img1.cols*scl, det_img1.rows*scl));
 	det_img1 = tmpl.clone();
@@ -283,9 +270,14 @@ int main(int argc, char * argv[])
 	vector<String> class_ids;
 
 	String id1 = "0";
-	
+
 	Point temp_offset1(400 * scl, 400 * scl);
 	Point temp_offset2(200 * scl, 400 * scl);
+
+
+	//Point temp_offset1(860 * scl, 860 * scl);
+	//Point temp_offset2(200 * scl, 400 * scl);
+
 	Point temp_offset;
 	double matrix4x4[16];
 	double matrix3x3[9];
@@ -294,21 +286,29 @@ int main(int argc, char * argv[])
 
 
 	/* 建立模板输入图像以及类别: eg: "id1, id2,id3..." */
-	Mat tmp_roi = tmpl(Rect(temp_offset1.x, temp_offset1.y, 200 * scl, 200 * scl));
-	Rect rc = Rect(temp_offset1.x, temp_offset1.y, 200 * scl, 200 * scl);
+	Rect tmp_roi = Rect(temp_offset1.x, temp_offset1.y, 200 * scl, 200 * scl);
+	vector<Rect> tmp_rois;
 
 	offset.push_back(temp_offset1);
-	img_tmpls.push_back(tmp_roi);
 	class_ids.push_back("id1");
 	img_dets.push_back(det_img1);
+	tmp_rois.push_back(tmp_roi);
 
 	if (trainFlag)
 	{/* 模板训练 */
+		vector<Mat> mask_rois;
 		cout << "train template..." << endl;
-		ret = my_templ.trainTemplate(img_tmpls, class_ids, offset);
+		TickMeter tick;
+		tick.start();
+		ret = my_templ.trainTemplate(img_dets, class_ids, tmp_rois, mask_rois);
+		//ret = my_templ.trainTemplateFast(img_dets, class_ids, tmp_rois);
 		if (ret <= 0)
 			cout << "train template error! please change \
 			the image size，filter_size or num_features size" << endl;
+
+		tick.stop();
+		//1. 33288ms
+		printf("trainning time:%.3f\n", tick.getTimeMilli());
 	}
 
 	rng = RNG((unsigned)time(NULL));
@@ -316,8 +316,11 @@ int main(int argc, char * argv[])
 	/* 模板检测任务开始 */
 	mvFPoint mm1, mm2;
 
-	vector <float> cc1, cc2;
+	vector <float> cc1, cc2, cc3;
 
+	float min1, max1, min3, max3;
+	min1 = max1 = min3 = max3 = 0;
+	int count = 0;
 	for (size_t ii = 0; ii < img_dets.size(); ii++)
 	{
 		double hmat[9];
@@ -329,31 +332,27 @@ int main(int argc, char * argv[])
 		Mat tmp_copy, tmp_gray;
 		Mat dst_copy, dst_gray;
 
-		for (float kk = 0; kk < 359; kk += 3)
+		for (float kk = 0; kk < 359; kk += 1.8)
 		{
 			int a = rng.uniform(20, 100);
 			int b = rng.uniform(20, 100);
 			Point2f oo = Point(a, a);
 			{
-				
 
-				mm1.x = (rc.tl().x + rc.br().x) / 2.0;
-				mm1.y = (rc.tl().y + rc.br().y) / 2.0;
+				mm1.x = (tmp_roi.tl().x + tmp_roi.br().x) / 2.0;
+				mm1.y = (tmp_roi.tl().y + tmp_roi.br().y) / 2.0;
 
-				img_roate = cv::getRotationMatrix2D(center + oo, kk, 1);
+				img_roate = cv::getRotationMatrix2D(center, kk, 1);//center:模板整图中心
 				det_img = img_dets[ii].clone();
 
 				//检测中坐标om1
-				mvMap2DPoint(mm1, (double*)img_roate.data, 0, mm2);
-
+				mvMap2DPoint(mm1, (double*)img_roate.data, 0, mm2);//mm2:模板区域中心绕模板整图中心旋转kk度后的坐标,即误差参考点
 
 				//circle(det_img, Point(mm2.x, mm2.y), 3, Scalar(0, 255, 0), -1);
 
 				det = det_img;
-
 				cv::warpAffine(det_img, det, img_roate, det_img.size());
 				circle(det, Point(mm2.x, mm2.y), 3, Scalar(0, 255, 0), -1);
-
 
 			}
 
@@ -367,7 +366,7 @@ int main(int argc, char * argv[])
 			int c1 = det.cols / 2;
 			int c2 = det.rows / 2;
 
-			p1 = Point(c1 - 120, c2 - 120);
+			p1 = Point(c1 - 60, c2 - 120);
 			ppnts.push_back(p1);
 			p1 = Point(c1 + 120, c2 - 120);
 			ppnts.push_back(p1);
@@ -387,20 +386,52 @@ int main(int argc, char * argv[])
 			ppnts.push_back(p1);
 			p1 = Point(c1 - 80, c2 + 80);
 			ppnts.push_back(p1);
-			
+
 			pls.push_back(ppnts);
 
-			/* 设置ROI检测区域，可以同时设置多个roi_index为ROI区域编号(1,2,3,4...)*/
-			my_templ.setDetRoi(pls, det.cols, det.rows);
+			static int  counter = 0;
+			counter++;
 
-			/* conf为相似度: 0~1.0 */
-			ret = my_templ.matchTemplate(det, class_ids, mres, conf);
-			if (ret < 0)
+			if (counter < 0)
+			{
+				counter = counter;
 				continue;
+			}
 
-			nTick2 = getTickCount();
-			ptime = ((double)nTick2 - nTick)*1000. / getTickFrequency();
-			/*printf("process time: %fms\n", ptime);*/
+			//if (counter > 15 && counter < 50)
+			//{ 
+			//	det = cv::Mat::zeros(det.rows, det.cols, det.type());
+			//	//continue;
+			//}
+			//else
+			//{
+			//	if (counter > 100)
+			//		counter = 0;
+			//}
+			//imshow("edge0", det);
+			//edgePreservingFilter(det, det, 1, 60.f, 0.4f);
+			//imshow("edge", det);
+			//waitKey();
+
+			/* 设置ROI检测区域，可以同时设置多个roi_index为ROI区域编号(1,2,3,4...)*/
+			//my_templ.setDetRoiMap(pls, det.cols, det.rows);
+
+			/* conf为相似度分数设置: 0~1.0 */
+			static int ffg = 0;
+			double avg_rough_time = 0.f;
+			if (ffg == 0)
+			{
+				TickMeter tick;
+				tick.start();
+				ret = my_templ.matchTemplate(det, class_ids, mres, conf);
+				tick.stop();
+
+				avg_rough_time = tick.getTimeMilli();
+
+			}
+
+			//nTick2 = getTickCount();
+			//ptime = ((double)nTick2 - nTick)*1000. / getTickFrequency();
 
 			tmp_copy = img_dets[0].clone();
 			dst_copy = det.clone();
@@ -415,10 +446,28 @@ int main(int argc, char * argv[])
 			else
 				dst_gray = dst_copy;
 
+			vector<matchResult>mres2 = mres;
+
+			Point2f pre_center;
+			float prev_angle;
+
+			if (mres.size() > 0)
+			{
+				pre_center = mres[0].bbox.center;
+				prev_angle = mres[0].bbox.angle;
+			}
+			TickMeter tick_refine;
+			tick_refine.start();
+			my_templ.refineObjectPos(dst_gray, mres);
+			tick_refine.stop();
+			double tt = tick_refine.getTimeMilli();
+			printf("alg match time: %f ms, icp refine time:%f ms, total = %f ms\n", avg_rough_time, tt, tt + avg_rough_time);
 			/* 显示结果匹配结果 */
+			int max_ffg = 0;
 			for (int jj = 0; jj < mres.size(); jj++)
 			{
 				matchResult obj = mres[jj];
+				//my_templ.refineObjectPos(dst_gray, obj);
 				vector <Point> pnts;
 
 				pnts.reserve(4);
@@ -426,12 +475,10 @@ int main(int argc, char * argv[])
 				pnts.push_back(Point(obj.bbox.pnts[1].x, obj.bbox.pnts[1].y));
 				pnts.push_back(Point(obj.bbox.pnts[2].x, obj.bbox.pnts[2].y));
 				pnts.push_back(Point(obj.bbox.pnts[3].x, obj.bbox.pnts[3].y));
-				std::vector<mvTemplate > template_feature;
+
 				vector<Point2f>  feature_points;
 
-				/* 优化matix3x3*/
-				my_templ.getAFTransform(obj, matrix3x3);
-				Mat affine_mat = Mat(Size(3, 2), CV_64FC1, (void*)matrix3x3);
+				Mat affine_mat = Mat(Size(3, 2), CV_64FC1, (void*)obj.tmat.pdata);
 
 				/* draw location */
 				for (int i = 0; i < 4; i++)
@@ -449,34 +496,27 @@ int main(int argc, char * argv[])
 						cv::putText(dst_copy, str, Point(10, 80), CV_FONT_HERSHEY_COMPLEX, 0.5, Scalar(0, 255, 0));
 				}
 
-				/* 针对匹配index进行，目标调整, obj.index = 0 为第0个匹配目标*/
-				my_templ.getTemplateFeatures(obj.class_id, 0, template_feature);
-				my_templ.getTemplateFeaturesPos(obj.class_id, template_feature[0], feature_points);
-
-				/* 目标位置优化调整 */
-				my_templ.refineObjectPos(tmp_gray, dst_gray, obj, feature_points, matrix3x3);
-
 				for (int i = 0; i < 4; i++)
 				{
 					int next = (i + 1 == 4) ? 0 : (i + 1);
-					if (obj.valid)
+					//if (obj.valid)
+					//{
+					//	cv::line(dst_copy, Point(obj.bbox.pnts[i].x, obj.bbox.pnts[i].y),
+					//		Point(obj.bbox.pnts[next].x, obj.bbox.pnts[next].y), Scalar(255, 0, 0), 2);
+					//	cv::line(my_templ.roi_map, Point(obj.bbox.pnts[i].x, obj.bbox.pnts[i].y),
+					//		Point(obj.bbox.pnts[next].x, obj.bbox.pnts[next].y), Scalar(60, 60, 60), 2);
+					//	circle(my_templ.roi_map, Point(obj.bbox.center.x, obj.bbox.center.y), 3, Scalar(60 * obj.roi_index, 60 * obj.roi_index, 60 * obj.roi_index), -1);
+					//}
+					//else
 					{
 						cv::line(dst_copy, Point(obj.bbox.pnts[i].x, obj.bbox.pnts[i].y),
-							Point(obj.bbox.pnts[next].x, obj.bbox.pnts[next].y), Scalar(255, 0, 0), 2);
-						cv::line(my_templ.roi_map, Point(obj.bbox.pnts[i].x, obj.bbox.pnts[i].y),
-							Point(obj.bbox.pnts[next].x, obj.bbox.pnts[next].y), Scalar(60, 60, 60), 2);
-						circle(my_templ.roi_map, Point(obj.r_center.x, obj.r_center.y), 3, Scalar(60 * obj.roi_index, 60 * obj.roi_index, 60 * obj.roi_index), -1);
-					}
-					else
-					{
-						cv::line(dst_copy, Point(obj.bbox.pnts[i].x, obj.bbox.pnts[i].y),
-							Point(obj.bbox.pnts[next].x, obj.bbox.pnts[next].y), Scalar(255, 255, 255), 2);
-						circle(my_templ.roi_map, Point(obj.r_center.x, obj.r_center.y), 3, Scalar(255, 255, 255), -1);
+							Point(obj.bbox.pnts[next].x, obj.bbox.pnts[next].y), Scalar(0, 255, 0), 2);
+						circle(my_templ.roi_map, Point(obj.bbox.center.x, obj.bbox.center.y), 3, Scalar(255, 255, 255), -1);
 					}
 					cv::String txt;
 					char str[512];
 
-					sprintf_s(str, "class_id:%s, score = %.2f, angle = %.1f(%.1f)", obj.class_id.c_str(), obj.score, my_templ.reffine_angle,
+					sprintf_s(str, "index: %d, score = %.2f, angle = %.1f(%.1f)", counter, obj.score, prev_angle,
 						obj.angle);
 					Point temp_offset;
 					if (obj.class_id == "id1")
@@ -486,37 +526,74 @@ int main(int argc, char * argv[])
 
 				}
 
-				float dif;
-				float a, b;
+				float dif, anglediff, dif0, anglediff0;
+				float a, b, a0, b0;
 
-				a = (mm2.x - obj.r_center.x) * (mm2.x - obj.r_center.x);
-				b = (mm2.y - obj.r_center.y) * (mm2.y - obj.r_center.y);
+				a0 = (mm2.x - pre_center.x) * (mm2.x - pre_center.x);
+				b0 = (mm2.y - pre_center.y) * (mm2.y - pre_center.y);
+				dif0 = sqrt((float)(a0 + b0));
+				anglediff0 = fabs(kk - prev_angle);
+
+
+				a = (mm2.x - obj.bbox.center.x) * (mm2.x - obj.bbox.center.x);
+				b = (mm2.y - obj.bbox.center.y) * (mm2.y - obj.bbox.center.y);
 
 				dif = sqrt((float)(a + b));
+				anglediff = fabs(kk - obj.angle);
+
+
 				cc1.push_back(dif);
 				cc2.push_back(dif / sqrt(2.0));
-				float sum1,sum2;
-				sum1 = sum2 = 0;
+				cc3.push_back(anglediff);
+
+
+				float sum1, sum2, sum3;
+				sum1 = sum2 = sum3 = 0;
 				for (int i = 0; i < cc1.size(); i++)
 				{
 					sum1 += cc1[i];
+
 					sum2 += cc2[i];
+					sum3 += cc3[i];
+
 				}
 
 				sum1 /= cc1.size();
 				sum2 /= cc2.size();
+				sum3 /= cc3.size();
 
-				printf("process time: %fms, distance = %.4f(%.4f), pix = %.4f(%.4f)  angle_dif = %.4f\n", ptime, dif, sum1, dif/sqrt(2.0), sum2, fabs(kk-obj.angle));
+				if (min1 > dif)
+				{
+					min1 = dif;
+				}
+				if (max1 < dif)
+				{
+					max1 = dif;
+				}
 
+				if (min3 > anglediff)
+				{
+					min3 = anglediff;
+				}
+				if (max3 < anglediff)
+				{
+					max3 = anglediff;
+				}
+				//printf("处理时间: %f 毫秒, 偏离真实目标 = %.4f(平均:%.4f)像素, pix = %.4f(%.4f)  angle_dif = %.4f\n", ptime, dif, sum1, dif/sqrt(2.0), sum2, fabs(kk-obj.angle));
+
+				printf("像素精度 = %.4f(%.4f)(平均:%.4f,最小:%.4f,最大:%.4f),角度精度 = %.4f(平均:%.4f,最小:%.4f,最大:%.4f),粗匹配耗时:%f, 精匹配耗时:%f\n", dif, dif0, sum1, min1, max1, anglediff, sum3, min3, max3, avg_rough_time, tick_refine.getTimeMilli());
+				count++;
 				DrawCoordTemplate(dst_copy, obj, scl);
 				circle(dst_copy, Point(mm2.x, mm2.y), 3, Scalar(0, 255, 0), -1);
+				if (dif > 12)
+					max_ffg = 1;
 			}
-			if (mres.size() ==  0)
-				printf("process time: %fms\n", ptime);
+			//if (mres.size() ==  0)
+			//	printf("process time: %fms\n", ptime);
 
 			cv::imshow("result", dst_copy);
-			for(int i = 0; i < pls.size(); i++)
-			{ 
+			for (int i = 0; i < pls.size(); i++)
+			{
 				vector <Point> ppnts;
 				ppnts = pls[i];
 				Point offset;
@@ -527,11 +604,15 @@ int main(int argc, char * argv[])
 			cv::imshow("roi_map", my_templ.roi_map);
 
 			cv::waitKey(2);
-
+			if (max_ffg)
+			{
+				waitKey();
+			}
 			if (kk > 355)
 				kk = 0;
 		}
 	}
+	cv::waitKey(0);
 
 	return 1;
 }
